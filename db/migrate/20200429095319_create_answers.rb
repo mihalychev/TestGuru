56462f775dfc164c8dab7b1a2f3fe46e4dfa574f
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-04-29 21:18:00 +0300

    fix

--- db/migrate/20200429095319_create_answers.rb
+++ db/migrate/20200429095319_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.string :text
-      t.boolean :correct
+      t.boolean :correct, null: false
 
       t.timestamps
     end
