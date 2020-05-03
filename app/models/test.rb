17e9419dde5932b91f04b22ef8e66c7fb4ddac45
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-05-03 17:25:09 +0300

    initial data in seeds and methods in User and Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.test_names(category_title)
+    Test.joins('JOIN categories ON tests.category_id = categories.id').where(categories: { title: category_title}).order(id: :desc).pluck(:title)
+  end
 end
