a607e30dffaad0ebb521119728999475697a12fc
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-06-08 12:57:24 +0300

    app logic

---
+++ db/migrate/20200604192929_create_test_passages.rb
@@ -0,0 +1,14 @@
+class CreateTestPassages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :test_passages do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :test, null: false, foreign_key: true
+      t.references :current_question, foreign_key: false
+      t.integer :correct_questions, default: 0
+
+      t.timestamps
+    end
+
+    add_foreign_key :test_passages, :questions, column: 'current_question_id'
+  end
+end
