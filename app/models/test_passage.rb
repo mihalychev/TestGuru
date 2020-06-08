352ea72c619fd270493f22946fbf07a11c1acf75
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-06-08 23:12:06 +0300

    fix

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,8 +3,7 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_validation :before_validation_set_next_question, on: :update
+  before_validation :before_validation_set_current_question
 
   def completed?
     current_question.nil?
@@ -19,25 +18,33 @@ class TestPassage < ApplicationRecord
   end
 
   def question_number
-    test.questions.index(current_question) + 1
-  end
-
-  def percentage
-    correct_questions.to_f / test.questions.count
+    test.questions.count - unpassed_questions.count
   end
 
   def complete_percentage 
     (percentage * 100).round
   end
 
-  private
-
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def successful?
+    percentage >= 0.85
   end
 
-  def before_validation_set_next_question
-    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+  private
+
+  def before_validation_set_current_question
+    self.current_question = next_question
+  end
+
+  def next_question
+    if current_question
+      unpassed_questions.first
+    else
+      test.questions.first
+    end
+  end
+
+  def unpassed_questions
+    test.questions.order(:id).where('id > ?', current_question.id)
   end
 
   def correct_answer?(answer_ids)
@@ -46,5 +53,9 @@ class TestPassage < ApplicationRecord
 
   def correct_answers
     current_question.answers.correct
+  end
+
+  def percentage
+    correct_questions.to_f / test.questions.count
   end
 end
